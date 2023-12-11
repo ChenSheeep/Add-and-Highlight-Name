ffd06be7256297d3a5d827740ac47c6f5bb96ed9
ChenSheeep <apple.inc.taiwan@gmail.com>
2023-12-11 22:15:47 +0800

    改成function

---
+++ read_highlight.py
@@ -0,0 +1,41 @@
+import re
+
+
+def get_name_dict(filepath):
+    # Open the file and read its content
+    with open(filepath, "r", encoding="utf-8") as file:
+        content = file.readlines()
+
+    # Initialize a dictionary to store names and members
+    name_dict = {}
+
+    # Iterate through each line in the file
+    for line in content:
+        # Split the line based on ":"
+        parts = line.split(":")
+        filename = parts[0].strip()
+
+        # Check if the filename contains English characters
+        contains_english = re.search("[a-zA-Z]", filename)
+        if contains_english:
+            # If English characters are present, take the first word as the representative
+            representative = filename.split()[0]
+        else:
+            # If no English characters, take the second word onward as the representative
+            representative = filename.split()[0][1:]
+
+        # Check if the line contains member information
+        if len(parts) == 2:
+            # If yes, split the members by ", " and store in the dictionary
+            member = parts[1].strip().split(", ")
+            name_dict[filename] = member
+        else:
+            # If no member information, store the representative in the dictionary
+            name_dict[filename] = representative
+    return name_dict
+
+
+# Print the final dictionary
+filepath = "members.txt"
+name_dict = get_name_dict(filepath)
+print(name_dict)
