8fa8b187853c77761e220be07cc32c3c0078b6bc
ChenSheeep <apple.inc.taiwan@gmail.com>
2023-12-11 22:44:03 +0800

    合併讀檔與highlight

--- highlight_name.py
+++ highlight_name.py
@@ -1,34 +1,91 @@
 import fitz  # PyMuPDF
+import re
+import os
+
+
+def parse_member_file(filepath):
+    """
+    Parse the member file and return a dictionary containing filenames and associated members or representatives.
+    """
+    # Open the file and read its content
+    with open(filepath, "r", encoding="utf-8") as file:
+        content = file.readlines()
+
+    # Initialize a dictionary to store filenames and corresponding members or representatives
+    name_dict = {}
+
+    # Iterate through each line in the file
+    for line in content:
+        # Split the line based on ":"
+        parts = line.split(":")
+        filename = parts[0].strip()
+
+        # Check if the filename contains English characters
+        contains_english = re.search("[a-zA-Z]", filename)
+        if contains_english:
+            # If English characters are present, take the first word as the representative
+            representative = filename.split()[0]
+        else:
+            # If no English characters, take the second word onward as the representative
+            representative = filename.split()[0][1:]
+
+        # Check if the line contains member information
+        if len(parts) == 2:
+            # If yes, split the members by ", " and store in the dictionary
+            members = parts[1].strip().split(", ")
+            name_dict[filename] = members
+        else:
+            # If no member information, store the representative in the dictionary
+            name_dict[filename] = [representative]
+    return name_dict
 
 
 def highlight_keywords(pdf_path, keywords, output_path):
-    # 打開PDF文件
+    """
+    Highlight keywords in the first page of a PDF and save the modified PDF.
+    """
+    # Open the PDF document
     pdf_document = fitz.open(pdf_path)
 
-    # 遍歷每一頁
-    for page_number in range(pdf_document.page_count):
-        page = pdf_document[page_number]
+    # Access the first page
+    page = pdf_document[0]
 
-        # 遍歷關鍵字列表
-        for keyword in keywords:
-            # 在頁面上搜索關鍵字
-            keyword_instances = page.search_for(keyword)
+    # Iterate through the list of keywords
+    for keyword in keywords:
+        # Search for the keyword on the page
+        keyword_instances = page.search_for(keyword)
 
-            # 遍歷每個關鍵字的實例並添加高亮
+        # Check if any instances of the keyword were found
+        if keyword_instances:
+            # Iterate through each instance and add a highlight annotation
             for inst in keyword_instances:
                 highlight = page.add_highlight_annot(inst)
 
-                # 設置高亮顏色（這裡設置為黃色），只設置描邊顏色
+                # Set highlight color (here set to yellow), only set stroke color
                 highlight.set_colors({"stroke": (1, 1, 0)})
 
-    # 保存修改後的PDF
-    pdf_document.save(output_path)
+            # Save the modified PDF and return (no need to continue checking other keywords)
+            pdf_document.save(output_path)
+            pdf_document.close()
+            return
+
+    # If no instances of any keyword were found, do not save the modified PDF
     pdf_document.close()
 
 
-# 示例使用
+# Print the final dictionary
+filepath = "members.txt"
 pdf_path = "test.pdf"
-output_path = "陳颺_highlighted.pdf"
-keywords = ["維芸"]
 
-highlight_keywords(pdf_path, keywords, output_path)
+# Create the output folder if it doesn't exist
+output_folder = "output"
+os.makedirs(output_folder, exist_ok=True)
+name_dict = parse_member_file(filepath)
+
+# Iterate through the filenames and associated members or representatives
+for filename, members_or_representative in name_dict.items():
+    # Generate the output path for the modified PDF
+    output_path = f"{output_folder}/{filename}.pdf"
+
+    # Highlight keywords on the first page and save the modified PDF
+    highlight_keywords(pdf_path, members_or_representative, output_path)
