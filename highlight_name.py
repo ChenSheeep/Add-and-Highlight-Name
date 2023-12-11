bae253eb7624954c686d7e3e468bc95d677ab8be
ChenSheeep <apple.inc.taiwan@gmail.com>
2023-12-11 22:12:00 +0800

    改為可以設定多個keywords

--- highlight_name.py
+++ highlight_name.py
@@ -1,7 +1,7 @@
 import fitz  # PyMuPDF
 
 
-def highlight_keywords(pdf_path, keyword, output_path):
+def highlight_keywords(pdf_path, keywords, output_path):
     # 打開PDF文件
     pdf_document = fitz.open(pdf_path)
 
@@ -9,15 +9,17 @@ def highlight_keywords(pdf_path, keyword, output_path):
     for page_number in range(pdf_document.page_count):
         page = pdf_document[page_number]
 
-        # 在頁面上搜索關鍵字
-        keyword_instances = page.search_for(keyword)
+        # 遍歷關鍵字列表
+        for keyword in keywords:
+            # 在頁面上搜索關鍵字
+            keyword_instances = page.search_for(keyword)
 
-        # 遍歷每個關鍵字的實例並添加高亮
-        for inst in keyword_instances:
-            highlight = page.add_highlight_annot(inst)
+            # 遍歷每個關鍵字的實例並添加高亮
+            for inst in keyword_instances:
+                highlight = page.add_highlight_annot(inst)
 
-            # 設置高亮顏色（這裡設置為黃色）
-            highlight.set_colors({"stroke": (1, 1, 0), "fill": (1, 1, 0)})
+                # 設置高亮顏色（這裡設置為黃色），只設置描邊顏色
+                highlight.set_colors({"stroke": (1, 1, 0)})
 
     # 保存修改後的PDF
     pdf_document.save(output_path)
@@ -25,8 +27,8 @@ def highlight_keywords(pdf_path, keyword, output_path):
 
 
 # 示例使用
-pdf_path = "陳颺.pdf"
+pdf_path = "test.pdf"
 output_path = "陳颺_highlighted.pdf"
-name = "陳颺"
+keywords = ["維芸"]
 
-highlight_keywords(pdf_path, name, output_path)
+highlight_keywords(pdf_path, keywords, output_path)
